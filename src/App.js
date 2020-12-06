734eda7b046380295fd69f1ffbf4561475d2f824
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-06 21:24:28 +0100

    add conpleted or not for each task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import './App.css';
-import TodoItem from './TodoItem/TodoItem.js';
-import todosData from "./todosData";
+import TodoItem from './TodoItem/TodoItem';
+import todosData from './todosData';
 
+// Import Style
+import './App.css';
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        })
+        return {
+            todos: updatedTodos
+        }
+    })
   }
 
   render() {
 
-    const todoComponents = this.state.todos.map(todo => <TodoItem key={todo.id} data={todo} />)
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 
     return (
-      <div className="app">
+      <div className='app'>
           <div className='to-do-items'>
-            {todoComponents}
+            {todoItems}
           </div>
       </div>
     )
--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-  const checkedBox = props.data.completed
   return (
-      <div className='to-do-item'>
-        <input onChange={() => console.log('coucou')} type="checkbox" checked={props.data.completed}/>
-        <p className='to-do-text'>{props.data.text}</p>
+    <div className='to-do-item'>
+        <input
+          type='checkbox'
+          checked={props.item.completed}
+          onChange={() => props.handleChange(props.item.id)}
+          />
+        <p className='to-do-text'>{props.item.text}</p>
       </div>
   )
 }
